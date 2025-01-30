b17b96c798973ec0351883f0d4a149ba684dfad3
ShadySoltan <shady.aly.soltan@gmail.com>
2025-01-30 18:22:53 +0200

    Recording and Stop Recording audio functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import FileDisplay from './Components/FileDisplay'
 import Header from './Components/Header'
 import HomePage from './Components/HomePage'
@@ -15,6 +15,10 @@ function App() {
     setFile(null)
     setAudioStream(null)
   }
+
+  useEffect(() => {
+    console.log(audioStream)
+  }, [audioStream])
 
   return (
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
