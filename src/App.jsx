09a180df21198d6698f8860c35883fd6c00a6eae
ShadySoltan <shady.aly.soltan@gmail.com>
2025-01-30 17:24:10 +0200

    FileDisplay page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
+import FileDisplay from './Components/FileDisplay'
 import Header from './Components/Header'
 import HomePage from './Components/HomePage'
 
 
 function App() {
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudiAvailable = file || audioStream
+
+  function handleAudioReset()
+  {
+    setFile(null)
+    setAudioStream(null)
+  }
 
   return (
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
 
-        <HomePage />
+        {isAudiAvailable ? (
+
+          <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream}/>
+
+        ) : ( 
+
+        <HomePage setFile={setFile} setAudioStream={setAudioStream} /> 
+
+        )}
+
       </section>
 
       <footer>
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-export default function HomePage() {
+export default function HomePage(props) {
+
+    const {setAudioStream, setFile} = props
+
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20'>
         <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Tran<span className='text-blue-400 bold'>scriptly</span></h1>
@@ -9,7 +12,10 @@ export default function HomePage() {
             <p className='text-blue-400'>Record</p>
             <i className='fa-solid fa-microphone'></i>
         </button>
-        <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>Upload <input className='hidden' type='file' accept='.mp3,.wave'/></label> a mp3 file</p>
+        <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>Upload <input onChange={(e) => {
+            const tempFile = e.target.files[0]
+            setFile(tempFile)
+        }} className='hidden' type='file' accept='.mp3,.wave'/></label> a mp3 file</p>
     </main>
   )
 }
