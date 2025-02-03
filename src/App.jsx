79c0cef2f777d560caa9b3c089d945255f1e1808
ShadySoltan <shady.aly.soltan@gmail.com>
2025-02-03 05:13:45 +0200

    phase 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import FileDisplay from './Components/FileDisplay'
 import Header from './Components/Header'
 import HomePage from './Components/HomePage'
+import Information from './Components/Information'
+import Transcribing from './Components/Transcribing'
+import { MessageTypes } from './utils/presets'
 
 
 function App() {
   const [file, setFile] = useState(null)
   const [audioStream, setAudioStream] = useState(null)
+  const [output, setOutput] = useState(null)
+  const [downloading, setDownloading] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [finished, setFinished] = useState(false)
 
-  const isAudiAvailable = file || audioStream
+  const isAudioAvailable = file || audioStream
 
   function handleAudioReset()
   {
@@ -16,23 +23,76 @@ function App() {
     setAudioStream(null)
   }
 
+  const worker = useRef(null)
+
   useEffect(() => {
-    console.log(audioStream)
-  }, [audioStream])
+    if (!worker.current) {
+      worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url), {
+        type: 'module'
+      })
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case 'DOWNLOADING':
+          setDownloading(true)
+          console.log('DOWNLOADING')
+          break;
+        case 'LOADING':
+          setLoading(true)
+          console.log('LOADING')
+          break;
+        case 'RESULT':
+          setOutput(e.data.results)
+          console.log(e.data.results)
+          break;
+        case 'INFERENCE_DONE':
+          setFinished(true)
+          console.log("DONE")
+          break;
+      }
+    }
+
+    worker.current.addEventListener('message', onMessageReceived)
+
+    return () => worker.current.removeEventListener('message', onMessageReceived)
+  })
+
+  async function readAudioFrom(file) {
+    const sampling_rate = 16000
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate })
+    const response = await file.arrayBuffer()
+    const decoded = await audioCTX.decodeAudioData(response)
+    const audio = decoded.getChannelData(0)
+    return audio
+  }
+
+  async function handleFormSubmission() {
+    if (!file && !audioStream) { return }
+
+    let audio = await readAudioFrom(file ? file : audioStream)
+    const model_name = `openai/whisper-tiny.en`
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name
+    })
+  }
 
   return (
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
 
-        {isAudiAvailable ? (
-
-          <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream}/>
-
-        ) : ( 
-
-        <HomePage setFile={setFile} setAudioStream={setAudioStream} /> 
-
+        {output ? (
+          <Information output={output} finished={finished}/>
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
+          <FileDisplay handleFormSubmission={handleFormSubmission} handleAudioReset={handleAudioReset} file={file} audioStream={audioStream} />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
         )}
 
       </section>
--- src/Components/FileDisplay.jsx
+++ src/Components/FileDisplay.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function FileDisplay(props) {
-    const {handleAudioReset, file, audioStream} = props
+    const {handleAudioReset, file, audioStream, handleFormSubmission} = props
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center pb-20 w-72 sm:w-96 max-w-full mx-auto'>
         <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
@@ -15,7 +15,7 @@ export default function FileDisplay(props) {
                 Reset
             </button>
 
-            <button className='specialBtn px-3 p-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium'>
+            <button onClick={handleFormSubmission} className='specialBtn px-3 p-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium'>
                 <p>Transcribe</p> 
                 <i className='fa-solid fa-pen-nib'></i>
             </button>
